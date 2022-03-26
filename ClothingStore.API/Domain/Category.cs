001492b649a1729eb703410248127a6e443e457d
Leticia-Mendes <mendesz.leticia@gmail.com>
2022-03-26 16:56:19 -0300

    domain and context

---
+++ ClothingStore.API/Domain/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothingStore.API.Domain
+{
+    [Table("Categories")]
+    public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+
+        [Key]
+        public int CategoryId { get; set; }
+        
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+        
+        [MaxLength(80)]
+        public string UrlImage { get; set; }
+        
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ ClothingStore.API/Domain/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothingStore.API.Domain
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        
+        [Required]
+        [StringLength(80)]
+        public string Name { get; set; }
+        
+        [Required]
+        [StringLength(300)]
+        public string Description { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+        
+        [StringLength(300)]
+        public string UrlImage { get; set; }
+        public string Stock { get; set; }
+        public DateTime RegisterDate  { get; set; }
+        
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
